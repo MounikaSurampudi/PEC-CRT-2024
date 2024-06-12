2c733f40fa3e62cf12283847d2faa40734a6e358
Surampudi Mounika <145670983+MounikaSurampudi@users.noreply.github.com>
2024-06-12 20:15:18 +0530

    Create Insertion Sort GfG

---
+++ Phase III/Day-8/Insertion Sort GfG.py
@@ -0,0 +1,43 @@
+#using iteration
+class Solution:
+    def insert(self, alist, index, n):
+        currEle = alist[index]
+        prevIndex = index - 1 
+        while prevIndex >= 0:
+            if alist[prevIndex] > currEle:
+                alist[prevIndex + 1] = alist[prevIndex]
+            else:
+                break 
+            prevIndex -= 1 
+        alist[prevIndex + 1] = currEle
+        
+        
+        
+    #Function to sort the list using insertion sort algorithm.    
+    def insertionSort(self, alist, n):
+        if n == 1:
+            return 
+        
+        for firstEleInUnsortedArr in range(1, n):
+            self.insert(alist, firstEleInUnsortedArr, n)
+
+#using recursion
+class Solution:
+    def insert(self, alist, index, n):
+        currEle = alist[index]
+        prevIndex = index - 1 
+        while prevIndex >= 0:
+            if alist[prevIndex] > currEle:
+                alist[prevIndex + 1] = alist[prevIndex]
+            else:
+                break 
+            prevIndex -= 1 
+        alist[prevIndex + 1] = currEle
+        
+    #Function to sort the list using insertion sort algorithm.    
+    def insertionSort(self, alist, n):
+        if n == 1:
+            return 
+        
+        self.insertionSort(alist, n - 1)
+        self.insert(alist, n - 1, n)

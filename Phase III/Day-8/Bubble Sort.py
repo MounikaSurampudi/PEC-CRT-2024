d52105f8f634d077d1cd42dc4324dd072f3a40fa
Surampudi Mounika <145670983+MounikaSurampudi@users.noreply.github.com>
2024-06-12 20:03:11 +0530

    Create Bubble Sort

---
+++ Phase III/Day-8/Bubble Sort.py
@@ -0,0 +1,25 @@
+#using recursion
+class Solution:
+    #Function to sort the array using bubble sort algorithm.
+    def bubbleSort(self,arr, n):
+        if n == 1:
+            return 
+        for index in range(n - 1):
+            if arr[index] > arr[index + 1]:
+                temp = arr[index]
+                arr[index] = arr[index + 1]
+                arr[index + 1] = temp 
+        self.bubbleSort(arr, n - 1)
+
+#using iterative solution
+class Solution:
+    #Function to sort the array using bubble sort algorithm.
+    def bubbleSort(self,arr, n):
+        lastIndex = n - 1 
+        while lastIndex > 0:
+            for index in range(lastIndex):
+                if arr[index] > arr[index + 1]:
+                    temp = arr[index]
+                    arr[index] = arr[index + 1]
+                    arr[index + 1] = temp 
+            lastIndex -= 1

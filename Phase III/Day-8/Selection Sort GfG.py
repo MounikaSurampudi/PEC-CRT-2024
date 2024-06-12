33e507797e6a4ae894f2aa28c185c4903aaf3031
Surampudi Mounika <145670983+MounikaSurampudi@users.noreply.github.com>
2024-06-12 20:09:34 +0530

    Create Selection Sort GfG

---
+++ Phase III/Day-8/Selection Sort GfG.py
@@ -0,0 +1,44 @@
+#using recursion
+class Solution: 
+    def select(self, arr, i):
+        largeEleIndex = i 
+        for index in range(i):
+            if arr[index] > arr[largeEleIndex]:
+                largeEleIndex = index 
+        return largeEleIndex
+    
+    def selectionSort(self, arr,n):
+        if n == 1:
+            return 
+        
+        
+        largeEleIndex = self.select(arr, n - 1)
+        if largeEleIndex != n - 1:
+            temp = arr[largeEleIndex]
+            arr[largeEleIndex] = arr[n - 1]
+            arr[n - 1] = temp 
+        
+        self.selectionSort(arr, n - 1)
+
+
+#using iteration
+class Solution: 
+    def select(self, arr, i):
+        largeEleIndex = i 
+        for index in range(i):
+            if arr[index] > arr[largeEleIndex]:
+                largeEleIndex = index 
+        return largeEleIndex
+    
+    def selectionSort(self, arr,n):
+        if n == 1:
+            return 
+        
+        lastIndex = n - 1 
+        while lastIndex > 0:
+            largeEleIndex = self.select(arr, lastIndex)
+            if largeEleIndex != lastIndex:
+                temp = arr[largeEleIndex]
+                arr[largeEleIndex] = arr[lastIndex]
+                arr[lastIndex] = temp 
+            lastIndex -= 1 

0382c1bbddfce275a5465ec491da9844dabe99b9
Surampudi Mounika <145670983+MounikaSurampudi@users.noreply.github.com>
2024-06-13 14:00:02 +0530

    Organized files

---
+++ Phase III/Day-8/Linear Search GfG.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search(self,arr, N, X):
+        result = -1 
+        for index in range(N):
+            if arr[index] == X:
+                result = index 
+                break
+        return result
